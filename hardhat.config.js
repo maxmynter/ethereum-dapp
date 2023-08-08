40e5967b13c3d9331bbb164faecea47ce6a825c0
Max Mynter <maxmynter@me.com>
2023-08-08 22:24:34 +0200

    First Smart Contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  const provider = hre.ethers.provider;
+
+  for (const account of accounts) {
+    console.log(
+      "%s (%i ETH)",
+      account.address,
+      hre.ethers.formatEther(
+        // getBalance returns wei amount, format to ETH amount
+        await provider.getBalance(account.address)
+      )
+    );
+  }
+});
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
